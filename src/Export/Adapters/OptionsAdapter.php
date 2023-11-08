8ad3c4f2a9445c8d996bb1f73efb1842a29eedc8
Bojan <bojan.dimitrijevic@soprex.com>
2023-11-08 15:26:52 +0100

    refactor optionsAdapter

--- src/Export/Adapters/OptionsAdapter.php
+++ src/Export/Adapters/OptionsAdapter.php
@@ -31,60 +31,6 @@ class OptionsAdapter
             return [];
         }
 
-        return $this->getPropertyGroupOptionAttributes($product->options);
-    }
-
-    /**
-     * @return Attribute[]
-     */
-    protected function getPropertyGroupOptionAttributes(PropertyGroupOptionCollection $collection): array
-    {
-        $attributes = [];
-
-        foreach ($collection as $propertyGroupOptionEntity) {
-            $group = $propertyGroupOptionEntity->group;
-            if ($group && !$group->filterable) {
-                continue;
-            }
-
-            $attributes = array_merge($attributes, $this->getAttributePropertyAsAttribute($propertyGroupOptionEntity));
-        }
-
-        return $attributes;
-    }
-
-    /**
-     * @return Attribute[]
-     */
-    protected function getAttributePropertyAsAttribute(PropertyGroupOptionEntity $propertyGroupOptionEntity): array
-    {
-        $attributes = [];
-
-        $group = $propertyGroupOptionEntity->group;
-        if ($group && $propertyGroupOptionEntity->getTranslation('name') && $group->getTranslation('name')) {
-            $groupName = $this->getAttributeKey($group->getTranslation('name'));
-            $propertyGroupOptionName = $propertyGroupOptionEntity->getTranslation('name');
-            if (!Utils::isEmpty($groupName) && !Utils::isEmpty($propertyGroupOptionName)) {
-                $propertyGroupAttrib = new Attribute($groupName);
-                $propertyGroupAttrib->addValue(Utils::removeControlCharacters($propertyGroupOptionName));
-
-                $attributes[] = $propertyGroupAttrib;
-            }
-        }
-
-        return $attributes;
-    }
-
-    /**
-     * For API Integrations, we have to remove special characters from the attribute key as a requirement for
-     * sending data via API.
-     */
-    protected function getAttributeKey(?string $key): ?string
-    {
-        if ($this->pluginConfig->isIntegrationTypeApi()) {
-            return Utils::removeSpecialChars($key);
-        }
-
-        return $key;
+        return Utils::getPropertyGroupOptionAttributes($product->options, $this->pluginConfig);
     }
 }
